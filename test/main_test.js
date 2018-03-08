8aba8268197084a7dc8189b7519f12cdd88c6ecb
eevaksu <eevaksu@gmail.com>
2018-03-08 12:38:07 +0000

    deleted all files

--- test/main_test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-//test suite
-describe ('Mocha', function() {
-  //test spec (unit test)
-  it('should run our tests using npm', function(){
-    expect(true).to.be.ok;
-
-  });
-});
